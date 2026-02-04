79e92d66d770c1daa6470f502033bcee4ae99695
Thewackker7 <b90533929@gmail.com>
2026-02-04 20:53:16 +0530

    Fix DATABASE_URL ParseError and resolve trivial conflict

--- config/settings.py
+++ config/settings.py
@@ -84,11 +84,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+if 'DATABASE_URL' in os.environ and os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 
 # Password validation
